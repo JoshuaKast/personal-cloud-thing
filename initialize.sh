fcddac06697816ff86c3bb2b32c78b6ec956d79e
JoshuaKast <jkast@mines.edu>
2021-02-03 15:09:22 -0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,15 +1,17 @@
 # personal cloud thing
 # shell script
-
+# Run as SUDO
 
 # Update/upgrade the rPi
-
+apt --assume-yes update
+apt --assume-yes upgrade
 
 
 # Install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker pi
 
-
-
-# Install MediaWiki
-
+docker version
+docker info
 
